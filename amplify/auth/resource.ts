c20cc8a05c2c7d93469f7f34299db53b64e77f72
Thomas Renken <torenken@gmail.com>
2025-01-24 23:15:01 +0100

    initial

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -7,5 +7,24 @@ import { defineAuth } from "@aws-amplify/backend";
 export const auth = defineAuth({
   loginWith: {
     email: true,
+
+    externalProviders: {
+      callbackUrls: ['http://localhost:3000'],
+      logoutUrls: ['http://localhost:3000'],
+    }
   },
+  userAttributes: {
+    email: {
+      required: true,
+      mutable: true
+    },
+    preferredUsername: {
+      required: false,
+      mutable: true
+    },
+    profilePicture: {
+      required: false,
+      mutable: true
+    }
+  }
 });
--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,13 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+backend.auth.resources.userPool.addDomain('cognito-domain', {
+  cognitoDomain: {
+    domainPrefix: 'dev-torenken-domain-github',
+  }
+})
